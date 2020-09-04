9fe68663ef7699a7824e9c697e678fe47e25ad85
Zambrana, Stephanie Maria <szambrana6@gatech.edu>
2020-09-04 19:06:32 -0400

    Creating omnipresent media player
    Creates omnipresent media player. Play/Pause buttons are still broken......

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import { MediaPlayer } from './components/MediaPlayer'
+
+// A BIT OF HARDCODING TESTING HERE TOO......
+let playDummy = () => {};
 
 const App: React.FC = () => (
   <>
     <StatusBar barStyle="dark-content" />
     <AppNavigator />
+    <MediaPlayer paused={false} mediaTitle="Song Title" playpause={playDummy}/>
   </>
 );
 
