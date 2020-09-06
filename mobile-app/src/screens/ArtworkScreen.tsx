df8a4a624948208e4881de25e733ba10a3c76831
kelseyhenson <kelseyrhenson@gmail.com>
2020-09-06 12:43:23 -0400

    Added ArtScreen

--- mobile-app/src/screens/ArtworkScreen.tsx
+++ mobile-app/src/screens/ArtworkScreen.tsx
@@ -1,18 +1,75 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Card from '../components/Card';
+import Title from '../components/Title';
+
+export default function ArtworkScreen(props) {
+  // Get exhibitList prop passed in from a Screen component in AppNavigator.
+  const exhibitList = props.route.params.exhibitList;
+
+  const tagList = [
+    ['#tag1', '#tag2'],
+    ['#tag1', '#tag2'],
+    ['#tag1'],
+    ['#tag1', '#tag2'],
+  ];
+  const colorsList = [
+    [2, 1],
+    [3, 2],
+    [4],
+    [1, 0],
+  ];
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.dummyPaddingTop} />
+        <Title text="Artwork" />
+        {
+          exhibitList.map((exhibit, index) => (
+            <Card
+              key={index}
+              navigation={props.navigation}
+              title={exhibit.title}
+              subtitle={exhibit.author}
+              bgImgURI={exhibit.images[0]}
+              tags={tagList[index]}
+              colors={colorsList[index]}
+            />
+          ))
+        }
+      </ScrollView>
+      <View style={styles.dummyPaddingBottom} />
+    </SafeAreaView>
+  );
+}
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
+  dummyPaddingTop: {
+    paddingTop: 24,
+  },
+  dummyPaddingBottom: {
+    paddingTop: 42,
+  },
+  scrollView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 24,
+    paddingBottom: 24,
+  }
 });
-
-const ArtworkScreen: React.FC = () => (
-  <View style={styles.container}>
-    <Text>Artwork Cards will ultimately be displayed here!</Text>
-  </View>
-);
-
-export default ArtworkScreen;
