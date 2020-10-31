72d8e449e1911d82424489ae0a4fd29910c594de
Nick Chaloult <npchaloult@gmail.com>
2020-10-30 21:31:41 -0400

    Clean up SearchBar

--- mobile-app/src/components/SearchBar.tsx
+++ mobile-app/src/components/SearchBar.tsx
@@ -1,11 +1,10 @@
-import { useLinkProps } from '@react-navigation/native';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { RootState } from '../store';
 import { StyleSheet, TextInput, View } from 'react-native';
 import { connect, ConnectedProps } from 'react-redux';
-import { RootState } from '../store';
 import { setSearchQuery } from '../store/artwork/actions';
 
-// Styles
+// Styles.
 
 const borderRadius = 18;
 
@@ -22,34 +21,30 @@ const styles = StyleSheet.create({
   },
 });
 
-// Component
-
-//Redux goodness.
+// Redux goodness.
 
 const mapState = (state: RootState) => ({
   search: state.artwork.searchQuery,
 });
-
 const mapDispatch = {
   setSearchQuery: (query: string) => setSearchQuery(query),
 };
 const connector = connect(mapState, mapDispatch);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-const SearchBar: React.FC<PropsFromRedux> = (props: PropsFromRedux) => {
+// Component.
 
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.search}
-        placeholder={"Search"}
-        value={props.search}
-        onChangeText={(newQuery) => {
-          props.setSearchQuery(newQuery);
-        }}
-      />
-    </View>
-  );
-};
+const SearchBar: React.FC<PropsFromRedux> = (props: PropsFromRedux) => (
+  <View style={styles.container}>
+    <TextInput
+      style={styles.search}
+      placeholder="Search"
+      value={props.search}
+      onChangeText={(newQuery) => {
+        props.setSearchQuery(newQuery);
+      }}
+    />
+  </View>
+);
 
 export default connector(SearchBar);
