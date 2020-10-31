c404f7a157a8ca42190ca7f98b809f6cd92b1f78
Nick Chaloult <npchaloult@gmail.com>
2020-10-30 21:40:38 -0400

    Clean up RootNavigation

--- mobile-app/src/navigation/RootNavigation.tsx
+++ mobile-app/src/navigation/RootNavigation.tsx
@@ -1,13 +1,10 @@
-
+import React from 'react';
 import { NavigationContainerRef } from '@react-navigation/native';
-import * as React from 'react';
-
 
 export const navigationRef: React.RefObject<NavigationContainerRef> = React.createRef();
 
 export function navigate(name: string): void {
-    if (navigationRef.current != null) {
-        navigationRef.current.navigate(name);
-    }
-//   navigationRef.current.navigate(name, params);
+  if (navigationRef.current != null) {
+    navigationRef.current.navigate(name);
+  }
 }
