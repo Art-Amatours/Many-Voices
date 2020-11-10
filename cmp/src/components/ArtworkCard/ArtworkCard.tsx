964599155571364dd2cd8c06b1f186f336942745
Nick Chaloult <npchaloult@gmail.com>
2020-11-09 22:17:38 -0500

    Say "critique" instead of "critiques" if numCritiques is 1

--- cmp/src/components/ArtworkCard/ArtworkCard.tsx
+++ cmp/src/components/ArtworkCard/ArtworkCard.tsx
@@ -14,7 +14,9 @@ const ArtworkCard: React.FC<Props> = (props) => (
       <div className="info">
         <div className="info-row">
           <span className="title">{props.title}</span>
-          <span className="num-critiques">{props.numCritiques} critiques</span>
+          <span className="num-critiques">
+            {props.numCritiques} critique{props.numCritiques !== 1 && 's'}
+          </span>
         </div>
         <div className="info-row">
           <span className="author">{props.artist}</span>
