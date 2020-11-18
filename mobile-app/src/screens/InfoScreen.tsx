f5af22be82811aa468e50bbb5e8b5d5c16ada578
DMReece <omreece@gmail.com>
2020-11-17 22:11:29 -0500

    pulled proper information from our database for the info screen

--- mobile-app/src/screens/InfoScreen.tsx
+++ mobile-app/src/screens/InfoScreen.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import CritiqueComponent from '../components/CritiqueComponent';
-import { Image, ScrollView, StyleSheet, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, View, Dimensions, Text } from 'react-native';
 import { RootState } from '../store';
 import { connect, ConnectedProps } from 'react-redux';
 
 // Styles.
+const deviceScreenHeight = Math.round(Dimensions.get('window').height);
+const deviceScreenWidth = Math.round(Dimensions.get('window').width);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  img: {
+  info: {
     width: '100%',
-    height: 300,
+    height: deviceScreenHeight
   },
+  image: {
+    width: deviceScreenWidth,
+    height: deviceScreenHeight,
+  },
+  description: {
+    marginTop: 0,
+    fontSize: 18,
+    fontWeight: '600',
+    paddingLeft: deviceScreenWidth/20,
+    paddingRight: deviceScreenWidth/20,
+    lineHeight: 30,
+  }
 });
 
 // Redux goodness.
@@ -28,10 +38,24 @@ type PropsFromRedux = ConnectedProps<typeof connector>;
 // Component.
 
 const InfoScreen: React.FC<PropsFromRedux> = (props: PropsFromRedux) => (
-  <View style={styles.container}>
-    <Image source={{ uri: props.artwork.imageURLs[0] }} style={styles.img} />
-   
-  </View>
+  <ScrollView
+  style ={styles.info}
+  showsVerticalScrollIndicator={false}>
+    <Image 
+        source={{ uri: props.artwork.imageURLs[0] }} 
+        style={styles.image} 
+        resizeMode={'contain'}
+    />
+    <Text 
+        style = {styles.description}> 
+        {props.artwork.artist}
+    </Text>
+    <Text 
+        style = {styles.description}> 
+        {props.artwork.description}
+    </Text>
+        
+  </ScrollView>
 );
 
 export default connector(InfoScreen);
