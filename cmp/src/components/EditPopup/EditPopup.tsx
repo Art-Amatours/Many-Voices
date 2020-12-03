40b03c093ff420b7ac4ee05b30f3ff5c928b6e60
Lee, Joseph Daniel <jlee3683@gatech.edu>
2020-12-03 00:49:00 -0500

    Tweaked Arangement and added Tag Modification

--- cmp/src/components/EditPopup/EditPopup.tsx
+++ cmp/src/components/EditPopup/EditPopup.tsx
@@ -2,11 +2,22 @@ import React, { useState } from 'react';
 import { EditorOptions } from 'typescript';
 import './styles.css';
 import { Artwork, Critique } from '../../store/artwork/types';
+import Card from '../Card/Card';
 import Tag from '../Tag/Tag';
 
 interface Props {
   artwork?: Artwork | null;
 }
+
+const colors = [
+  '#5fe382',
+  '#3fd9c2',
+  '#7589eb',
+  '#c55bcf',
+  '#a4db81',
+  '#e6b04c',
+  '#ed9c7b',
+];
 
 const EditPopup: React.FC<Props> = (props: Props) => {
   const [title, setTitle] = useState(props.artwork?.title ?? '');
@@ -15,6 +26,7 @@ const EditPopup: React.FC<Props> = (props: Props) => {
   const [imageURLs, setImageURLS] = useState(props.artwork?.imageURLs ?? ['']);
   const [file, setFile] = useState<File | null>(null);
   const [tags, setTags] = useState(props.artwork?.tags ?? [['', '']]);
+  const [newTag, setnewTag] = useState('');
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -22,42 +34,101 @@ const EditPopup: React.FC<Props> = (props: Props) => {
   };
 
   return (
-    <div className="create-artwork-screen-container">
-      <span className="create-artwork-header">Create a New Artwork</span>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          name="artist"
-          value={artist}
-          onChange={(e) => setArtist(e.target.value)}
-        />
-        <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
-        <img src={imageURLs[0]} alt="artwork" />
-        <input
-          type="file"
-          name="file"
-          onChange={(e) => {
-            if (e.target.files && e.target.src) {
-              setImageURLS([e.target.src]);
-              setFile(e.target.files[0]);
-            }
-          }}
-        />
-        {tags.map((tag) => (
-          <>
-            <Tag name={tag[0]} backgroundColor={tag[1]} />
-            <input type="button" value="X" />
-          </>
-        ))}
-        <input type="submit" value="Create" />
-      </form>
+    <Card>
+      {/* <div className="create-artwork-screen-container">
+      <div className="info">
+        <div className="info-row">
+          <span className="title">{props.title}</span>
+          <span className="num-critiques">
+            {props.numCritiques} critique{props.numCritiques !== 1 && 's'}
+          </span>
+        </div>
+        <div className="info-row">
+          <span className="author">{props.artist}</span>
+        </div>
+        <div className="tag-row">
+          {props.tagData.map(([name, backgroundColor]) => (
+            <Tag name={name} backgroundColor={backgroundColor} />
+          ))}
+        </div>
+      </div>
+      <img src={props.imageURLs[0]} alt={`Artwork: ${props.title}`} />
     </div>
+ */}
+
+      <div className="create-artwork-screen-container">
+        <span className="create-artwork-header">Create a New Artwork</span>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="info-row">
+            <input
+              className="title"
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="info-row">
+            <input
+              className="author"
+              type="text"
+              name="artist"
+              value={artist}
+              onChange={(e) => setArtist(e.target.value)}
+            />
+          </div>
+          {tags.map((tag) => (
+            <>
+              <Tag name={tag[0]} backgroundColor={tag[1]} />
+              <input
+                type="button"
+                value="X"
+                onClick={() => {
+                  setTags(
+                    tags
+                      .slice(0, tags.indexOf(tag))
+                      .concat(tags.slice(tags.indexOf(tag) + 1, tags.length)),
+                  );
+                }}
+              />
+            </>
+          ))}
+          <input
+            className="title"
+            type="text"
+            name="tag"
+            value={newTag}
+            onChange={(e) => setnewTag(e.target.value)}
+          />
+          <input
+            type="button"
+            value="+"
+            onClick={() => {
+              setTags(
+                tags.concat([
+                  [newTag, colors[Math.floor(Math.random() * colors.length)]],
+                ]),
+              );
+            }}
+          />
+          <img src={imageURLs[0]} alt="artwork" />
+          <input
+            type="file"
+            name="file"
+            onChange={(e) => {
+              if (e.target.files) {
+                setImageURLS([URL.createObjectURL(e.target.files[0])]);
+                setFile(e.target.files[0]);
+              }
+            }}
+          />
+          <div className="info-row">
+            <textarea value={desc} onChange={(e) => setDesc(e.target.value)} />
+          </div>
+          <input type="submit" value="Create" />
+        </form>
+      </div>
+    </Card>
   );
 };
 
